66892d3b90ddeeb067b7b4511953dc55de07e45c
Wilker <wilkermizael@gmail.com>
2023-07-06 12:15:41 -0300

    feito post cadastro

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-import express from "express"
-import cors from "cors"
-import {MongoClient} from "mongodb"
-//import Joi from "joi"
-import dotenv from "dotenv"
+import express from 'express'
+import cors from 'cors'
+import {MongoClient} from 'mongodb'
+import joi from 'joi'
+import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
 
 //CRIAÇÃO DO APP
 
@@ -21,9 +22,32 @@ try{
 }catch(err){
     console.log(err.message)
 }
-const db = mongoClient.db()
+const db = mongoClient.db('Mywallet')
+
 
 //ROTAS
-app.get()
+const schemaCadastro = joi.object({
+    nome: joi.string().required(),
+    email: joi.string().email().required(),
+    senha: joi.string().required()
+})
+app.post('/cadastro', async (req, res) =>{
+    const {nome, senha, email} = req.body
+    const passwordCrypt = bcrypt.hashSync(senha,2)
+
+    const validation = schemaCadastro.validate(req.body)
+    
+    if(validation.error) return res.status(422).send('Dados invalidos')
+    
+    try{
+        const usuario = await db.collection('users').findOne({email})
+        if(usuario) return res.status(422).send('Usuário ja cadastrado')
+        await db.collection('users').insertOne({nome, email, senha:passwordCrypt})
+        return res.status(201).send('Criado com sucesso')
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
+
+})
 const port = process.env.PORT || 5000
 app.listen(port, ()=>console.log(`Servidor rodando na porta ${port}`))
